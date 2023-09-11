68753c906242484b8a766a7eb2d90631d3cd1813
Jagna Hendriks <jagna@Laptop-JH.local>
2023-09-11 16:30:55 +0200

    deleted

--- .config/nvim/lua/user/autocmd.lua
+++
@@ -1,2 +0,0 @@
--- also closing nvimtree when closing last buffer
--- vim.api.nvim_exec([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]], false)
